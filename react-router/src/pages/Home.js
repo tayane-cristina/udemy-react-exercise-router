0271f5b2802d0db8d6c0e7c7af62a8f3629a8deb
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2023-09-25 09:23:19 -0300

    home concluide

--- react-router/src/pages/Home.js
+++ react-router/src/pages/Home.js
@@ -21,6 +21,31 @@ const Home = () => {
             </div>
             <span className='card-counter'>1/3</span>
         </div>
+        <div className="third-page full-page">
+            <div className="invite-text">
+                <h3 className="home-h3">Come <span className="word-key">visit us</span> and <span className="word-key">be part</span> of our community</h3>
+                <button className="button-invite"><img className="invite-img-whats" src="https://cdn-icons-png.flaticon.com/128/3670/3670051.png?ga=GA1.1.2089675641.1676983403&track=ais" alt="WhatsApp icon"/> WhatsApp</button>
+            </div>
+            <div className="gallery-image">
+                <img className="img-glr" src="https://images.pexels.com/photos/2040189/pexels-photo-2040189.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="gallery"></img>
+                <img className="img-glr" src="https://images.pexels.com/photos/2521978/pexels-photo-2521978.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="gallery"></img>
+                <img className="img-glr" src="https://images.pexels.com/photos/2014809/pexels-photo-2014809.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="gallery"></img>
+                <img className="img-glr" src="https://images.pexels.com/photos/1804638/pexels-photo-1804638.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="gallery"></img>
+                <img className="img-glr" src="https://images.pexels.com/photos/2076932/pexels-photo-2076932.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="gallery"></img>
+                <img className="img-glr" src="https://images.pexels.com/photos/1813346/pexels-photo-1813346.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="gallery"></img>
+                <img className="img-glr" src="https://images.pexels.com/photos/2068672/pexels-photo-2068672.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="gallery"></img>
+            </div>
+        </div>
+        <footer>
+            <div className="social-midia">
+                <img className="social-midia-icon" alt="WhatsApp" src="https://cdn-icons-png.flaticon.com/128/3670/3670051.png?ga=GA1.1.2089675641.1676983403&track=ais" ></img>
+                <img className="social-midia-icon" alt="instagram" src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png?ga=GA1.1.2089675641.1676983403&track=ais" ></img>
+                <img className="social-midia-icon" alt="facebook" src="https://cdn-icons-png.flaticon.com/128/3670/3670124.png?ga=GA1.1.2089675641.1676983403&track=ais" ></img>
+                <img className="social-midia-icon" alt="twitter" src="https://cdn-icons-png.flaticon.com/128/3670/3670211.png?ga=GA1.1.2089675641.1676983403&track=ais" ></img>
+            </div>
+            <p className="adress">Centro / Copacabana / Leblon / Tijuca / Ipanema</p>
+            <p>Página desenvolvida por Tayane Cristina em 25 de setembro de 2023 - créditos de imagem a freepik e pixels</p>
+        </footer>
         
     </div>
 };
